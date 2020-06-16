96ea9129b9465844dd2dec4a3e179d5654286708
川島慎悟 <kawashingo1990@gmail.com>
2020-06-16 19:20:54 +0900

    enum型

--- typescript_type/index.ts
+++ typescript_type/index.ts
@@ -24,3 +24,17 @@ console.log(person)
 const fruits: string[] = ['Apple', 'Banana', 'Grape']
 
 const book: [string, number, boolean] = ['business', 1500, false]
+
+// enum型は大文字から始める・中身は全て大文字にするのが慣習
+enum CoffeeSize {
+  SHORT = 'SHORT',
+  TALL = 'TALL',
+  GRANDE = 'GRANDE',
+  VENTI = 'VENTI'
+}
+
+const coffee = {
+  hot: true,
+  size: CoffeeSize.TALL
+}
+coffee.size
