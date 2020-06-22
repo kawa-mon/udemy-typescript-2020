21a85b2d9568d7edac5a2182041183ff2659fbfc
川島慎悟 <kawashingo1990@gmail.com>
2020-06-22 10:20:44 +0900

    interface で関数の型定義

--- src/interface.ts
+++ src/interface.ts
@@ -1,5 +1,14 @@
 export {}
 
+// type addFunc = (num1: number, num2: number) => number;
+interface addFunc {
+  (num1: number, num2: number): number
+}
+let addFunc: addFunc
+addFunc = (n1: number, n2: number) => {
+  return n1 + n2
+}
+
 interface Nameable {
   readonly name: string
 }
