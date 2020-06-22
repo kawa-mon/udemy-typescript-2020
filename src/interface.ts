3b33ad34f9f5dc66771ab770c6a9e157f3dbee8e
川島慎悟 <kawashingo1990@gmail.com>
2020-06-22 10:31:46 +0900

    ?オプション

--- src/interface.ts
+++ src/interface.ts
@@ -11,18 +11,27 @@ addFunc = (n1: number, n2: number) => {
 
 interface Nameable {
   readonly name: string
+  nickname?: string
 }
 interface Human extends Nameable {
   age: number
-  greeting(message: string): void
+  greeting(message?: string): void
 }
 
 class Developer implements Human {
-  constructor(public name: string, public age: number, public experience: number) {}
-  greeting(message: string) {
-    console.log(`Hello! ${message}`)
+  constructor(
+    public name: string,
+    public age: number,
+    public experience: number,
+    public nickname?: string
+  ) {}
+  greeting(message?: string) {
+    if (!!message) {
+      console.log(`Hello! ${message}.`)
+    }
+    console.log(`I am ${this.name}. ${this.age} years old. `)
   }
 }
-const developer: Human = new Developer('Quill', 38, 5)
-console.log({ developer })
+const developer: Human = new Developer('Quill', 38, 5, 'Q')
 console.log(developer.greeting('Bob'))
+console.log(developer.greeting())
