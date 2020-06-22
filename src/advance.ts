13ed1318e5377ffbcb9face293315750395294d2
川島慎悟 <kawashingo1990@gmail.com>
2020-06-23 06:17:52 +0900

    型ガード

--- src/advance.ts
+++ src/advance.ts
@@ -16,3 +16,33 @@ const quill: EngineerBlogger = {
   role: 'front-end',
   follower: 1000
 }
+
+function toUpperCase(x: string | number) {
+  if (typeof x === 'string') return x.toUpperCase()
+  return ''
+}
+
+type NomadWorker = Engineer | Blogger
+function describeProfile(nomadWorker: NomadWorker) {
+  if ('role' in nomadWorker) console.log(nomadWorker.role)
+  if ('follower' in nomadWorker) console.log(nomadWorker.follower)
+}
+
+class Dog {
+  speak(): void {
+    console.log('bow-bow')
+  }
+}
+class Bird {
+  speak(): void {
+    console.log('tweet-tweet')
+  }
+  fly(): void {
+    console.log('flutter')
+  }
+}
+type Pet = Dog | Bird
+function havePet(pet: Pet): void {
+  if (pet instanceof Bird) pet.fly()
+}
+havePet(new Bird())
