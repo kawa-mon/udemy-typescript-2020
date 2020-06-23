3f64c5cab51ccddae2da6b9b612b2efea8bdfc02
川島慎悟 <kawashingo1990@gmail.com>
2020-06-24 08:53:14 +0900

    関数型のインターセクションもオーバーロードを用いる

--- src/advance.ts
+++ src/advance.ts
@@ -34,6 +34,18 @@ const realFunc: TmpFunc = function (x: string | number) {
   return 0
 }
 
+interface FuncA {
+  (a: number, b: string): number
+  (a: string, b: number): number
+}
+interface FuncB {
+  (a: string): number
+}
+let intersectionFunc: FuncA & FuncB
+intersectionFunc = function (a: number | string, b?: number | string) {
+  return 0
+}
+
 type NomadWorker = Engineer | Blogger
 function describeProfile(nomadWorker: NomadWorker) {
   if ('role' in nomadWorker) console.log(nomadWorker.role)
