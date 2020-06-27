2568f8592c9d92e5f41bb1aef21f5b225a462f5f
川島慎悟 <kawashingo1990@gmail.com>
2020-06-28 08:18:04 +0900

    dev, prod で分ける

---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/food-app/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+}
